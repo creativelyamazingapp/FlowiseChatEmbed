6eb59d09f2b61d978bb56a6c214b6736cead33be
Sunil <sungem007@gmail.com>
2023-10-06 23:32:58 +0530

    branding

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -54,14 +54,14 @@ export const Badge = (props: Props) => {
     }}>Made by
       <a
         ref={liteBadge}
-        href={'https://flowiseai.com'}
+        href={'https://airockers.com'}
         target="_blank"
         rel="noopener noreferrer"
         class="lite-badge"
         id="lite-badge"
         style={{ "font-weight": 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span> AI Rockers</span>
       </a>
     </span>
   )
